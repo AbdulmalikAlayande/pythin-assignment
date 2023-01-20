caf3a72534e1ee35b871a88fbe33655c9ad672cc
Abdulmalik Alayande <alaabdulmalik03@gmail.com>
2023-01-20 08:52:52 +0100

    first comiit

---
+++ ass2.py
@@ -0,0 +1,11 @@
+#if one inch of rain falls on an acre of land how many gallons of water 💦 per acre 
+# acre of land.
+
+inch_rainfall = print("enter amount of rainfall: ")
+
+inch_rainfall_amount = int("inch_rainfall ")
+inch_rainfall_amt = float("inch_rainfall")
+acre_sq_ft = 43460
+gallons_per_cubic_ft = 7.48
+gallons = float(inch_rainfall_amt * acre_sq_ft) * gallons_per_cubic_ft
+print(gallons)
